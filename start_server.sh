17a309ee66a4870fc86e312c8965dce8d5ccaa1f
linzhi <linzhilynn@gmail.com>
2019-02-07 10:19:35 +0800

    fix

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-orterun -np 1 --host localhost --enable-recovery --repo-uri /tmp/urifile daos_server > /dev/null 2>&1 &
+orterun -np 1 --host localhost --enable-recovery --report-uri /tmp/urifile daos_server > /dev/null 2>&1 &
